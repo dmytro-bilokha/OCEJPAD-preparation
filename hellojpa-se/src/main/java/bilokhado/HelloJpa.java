2f5405c3a5431b2401c355eee22a08cdbc8fcde9
Dmytro Bilokha <dmytro@posteo.net>
2016-10-17 21:20:49 +0300

    Added simple logging

--- hellojpa-se/src/main/java/bilokhado/HelloJpa.java
+++ hellojpa-se/src/main/java/bilokhado/HelloJpa.java
@@ -1,9 +1,15 @@
 package bilokhado;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class HelloJpa {
 
+    private static final Logger logger = LoggerFactory.getLogger(HelloJpa.class);
+
     public static void main(String[] cmdLineParams) {
-	System.out.println("Hello World");
+        System.out.println("Hello World");
+        logger.info("Hello World logged");
     }
 
 }
