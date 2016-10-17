efe7b5503e1832c5b668ce9befba5d6b10fe480c
Dmytro Bilokha <dmytro@posteo.net>
2016-10-17 22:00:50 +0300

    Included H2 Database

--- hellojpa-se/src/main/java/bilokhado/HelloJpa.java
+++ hellojpa-se/src/main/java/bilokhado/HelloJpa.java
@@ -3,6 +3,10 @@ package bilokhado;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class HelloJpa {
 
     private static final Logger logger = LoggerFactory.getLogger(HelloJpa.class);
@@ -10,6 +14,24 @@ public class HelloJpa {
     public static void main(String[] cmdLineParams) {
         System.out.println("Hello World");
         logger.info("Hello World logged");
+        try {
+            Class.forName("org.h2.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        Connection conn = null;
+        try {
+            conn = DriverManager.
+                    getConnection("jdbc:h2:~/test", "sa", "");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        // add application code here
+        try {
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
 }
