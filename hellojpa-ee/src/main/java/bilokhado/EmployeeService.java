264ce50c4a686397f6242d6b725685f92cd57fb5
Dmytro Bilokha <dmytro@posteo.net>
2016-10-23 17:05:13 +0300

    Added ejb service to access DB

--- hellojpa-ee/src/main/java/bilokhado/EmployeeService.java
+++ hellojpa-ee/src/main/java/bilokhado/EmployeeService.java
@@ -1,9 +1,23 @@
 package bilokhado;
 
 
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
 /**
  * Created by dimon on 22.10.16.
  */
+@Stateless
 public class EmployeeService {
 
+    @PersistenceContext(unitName="hellojpa-ee")
+    private EntityManager em;
+
+    public List<Employee> findAllEmployees() {
+        TypedQuery<Employee> query = em.createQuery("SELECT e FROM Employee e", Employee.class);
+        return query.getResultList();
+    }
 }
--- hellojpa-ee/src/main/java/bilokhado/HelloJpaServlet.java
+++ hellojpa-ee/src/main/java/bilokhado/HelloJpaServlet.java
@@ -3,6 +3,7 @@ package bilokhado;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.ejb.EJB;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -14,6 +15,17 @@ import java.io.PrintWriter;
 public class HelloJpaServlet extends HttpServlet {
 
     private static final Logger LOG = LoggerFactory.getLogger(HelloJpaServlet.class);
+    private static final String OUTPUT_START = "<!DOCTYPE html PUBLIC '-//W3C//DTD HTML 4.01 Transitional//EN'"
+        + " 'http://www.w3.org/TR/html4/loose.dtd'>"
+            + "<html lang='en'>"
+            + "<head><title>Hello JPA EE</title></head>"
+            + "<body><table><thead><tr><th>Id</th><th>First Name</th><th>Last Name</th></thead>"
+            + "<tbody>";
+    private static final String OUTPUT_END = "</tbody></table></body></html>";
+    private static final String OUTPUT_LINE_TEMPLATE = "<tr><th>%d</th><th>%s</th><th>%s</th></tr>";
+
+    @EJB
+    private EmployeeService employeeService;
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) {
@@ -22,9 +34,11 @@ public class HelloJpaServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        LOG.info("In doGet method");
         PrintWriter out = response.getWriter();
-        out.println("Hello from Jpa Servlet!");
+        out.println(OUTPUT_START);
+        employeeService.findAllEmployees().forEach(emp -> out.println(String.format(OUTPUT_LINE_TEMPLATE
+                , emp.getId(), emp.getFirstName(), emp.getLastName())));
+        out.println(OUTPUT_END);
     }
 
 }
