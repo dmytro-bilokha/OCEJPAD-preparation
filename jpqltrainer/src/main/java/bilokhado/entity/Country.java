a53f62bb8314af1ceb98768a29cc8986304eb111
Dmytro Bilokha <dmytro@posteo.net>
2016-10-30 22:07:51 +0200

    Added Country entity

---
+++ jpqltrainer/src/main/java/bilokhado/entity/Country.java
@@ -0,0 +1,193 @@
+package bilokhado.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * Class represents the Country entity
+ */
+@Entity
+@Table(name = "country")
+public class Country {
+
+    @Id
+    @Column(name = "Code")
+    private String code;
+
+    @Column(name = "Name")
+    private String name;
+
+    @Column(name = "Continent")
+    private String continent;
+
+    @Column(name = "Region")
+    private String region;
+
+    @Column(name = "SurfaceArea")
+    private String surfaceArea;
+
+    @Column(name = "IndepYear")
+    private Short independencyYear;
+
+    @Column(name = "Population")
+    private int population;
+
+    @Column(name = "LifeExpectancy")
+    private float lifeExpectancy;
+
+    @Column(name = "GNP")
+    private double gnp;
+
+    @Column(name = "GNPOld")
+    private double oldGnp;
+
+    @Column(name = "LocalName")
+    private String localName;
+
+    @Column(name = "GovernmentForm")
+    private String governmentForm;
+
+    @Column(name = "HeadOfState")
+    private String headOfState;
+
+    @Column(name = "Capital")
+    private Integer capital;
+
+    @Column(name = "Code2")
+    private String shortCode;
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Country[");
+        sb.append("code='").append(code).append('\'');
+        sb.append(", name='").append(name).append('\'');
+        sb.append(", population=").append(population);
+        sb.append(", lifeExpectancy=").append(lifeExpectancy);
+        sb.append(", gnp=").append(gnp);
+        sb.append(']');
+        return sb.toString();
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContinent() {
+        return continent;
+    }
+
+    public void setContinent(String continent) {
+        this.continent = continent;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getSurfaceArea() {
+        return surfaceArea;
+    }
+
+    public void setSurfaceArea(String surfaceArea) {
+        this.surfaceArea = surfaceArea;
+    }
+
+    public Short getIndependencyYear() {
+        return independencyYear;
+    }
+
+    public void setIndependencyYear(Short independencyYear) {
+        this.independencyYear = independencyYear;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+    public float getLifeExpectancy() {
+        return lifeExpectancy;
+    }
+
+    public void setLifeExpectancy(float lifeExpectancy) {
+        this.lifeExpectancy = lifeExpectancy;
+    }
+
+    public double getGnp() {
+        return gnp;
+    }
+
+    public void setGnp(double gnp) {
+        this.gnp = gnp;
+    }
+
+    public double getOldGnp() {
+        return oldGnp;
+    }
+
+    public void setOldGnp(double oldGnp) {
+        this.oldGnp = oldGnp;
+    }
+
+    public String getLocalName() {
+        return localName;
+    }
+
+    public void setLocalName(String localName) {
+        this.localName = localName;
+    }
+
+    public String getGovernmentForm() {
+        return governmentForm;
+    }
+
+    public void setGovernmentForm(String governmentForm) {
+        this.governmentForm = governmentForm;
+    }
+
+    public String getHeadOfState() {
+        return headOfState;
+    }
+
+    public void setHeadOfState(String headOfState) {
+        this.headOfState = headOfState;
+    }
+
+    public Integer getCapital() {
+        return capital;
+    }
+
+    public void setCapital(Integer capital) {
+        this.capital = capital;
+    }
+
+    public String getShortCode() {
+        return shortCode;
+    }
+
+    public void setShortCode(String shortCode) {
+        this.shortCode = shortCode;
+    }
+
+}
